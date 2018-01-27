9c52e7c53aa8c8fb5573bf45b4eae9dabbeada5e
Tim Foerster <tim.foerster@hetzner.com>
2018-01-28 00:08:52 +0100

    Add server create handler
    Signed-off-by: Tim Foerster <tim.foerster@hetzner.com>

--- lib/hcloud/server_resource.rb
+++ lib/hcloud/server_resource.rb
@@ -2,6 +2,25 @@ module Hcloud
   class ServerResource < AbstractResource
     include Enumerable
 
+    def create(name:,
+               server_type:,
+               datacenter: nil,
+               location: nil,
+               start_after_create: nil,
+               image:,
+               ssh_keys: [],
+               user_data: nil)
+      query = {}
+      method(:create).parameters.inject(query) do |r,x| 
+        (var = eval(x.last.to_s)).nil? ? r : r.merge!(x.last => var)
+      end
+      Server.new(
+        Oj.load(request("servers", j: query, code: 200).run.body)["server"],
+        self,
+        client
+      )
+    end
+
     def all
       Oj.load(request("servers").run.body)["servers"].map do |x|
         Server.new(x, self, client)
