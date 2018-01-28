52f504ea7bbe21cf14079e7bb5eb35da623d5678
Tim Foerster <tim.foerster@hetzner.com>
2018-01-28 15:03:17 +0100

    Add server actions
    Signed-off-by: Tim Foerster <tim.foerster@hetzner.com>

--- lib/hcloud/server.rb
+++ lib/hcloud/server.rb
@@ -29,20 +29,61 @@ module Hcloud
     end
 
     def destroy
-      Action.new(
-        Oj.load(request(base_path, method: :delete).run.body)["action"],
-        parent,
-        client
-      )
+      action(request(base_path, method: :delete))[0]
     end
-   
-    %w(poweron poweroff shutdown reboot reset disable_rescue).each do |action|
+
+    def enable_rescue(type: "linux64", ssh_keys: [])
+      query = {}
+      method(:enable_rescue).parameters.inject(query) do |r,x| 
+        (var = eval(x.last.to_s)).nil? ? r : r.merge!(x.last => var)
+      end
+      a , j = action(request(base_path("actions/enable_rescue"), j: query))
+      [a, j["root_password"]]
+    end
+    
+    def reset_password
+      a , j = action(request(base_path("actions/reset_password"), method: :post))
+      [a, j["root_password"]]
+    end
+    
+    def create_image(description: nil, type: nil)
+      query = {}
+      method(:create_image).parameters.inject(query) do |r,x| 
+        (var = eval(x.last.to_s)).nil? ? r : r.merge!(x.last => var)
+      end
+      a , j = action(request(base_path("actions/create_image"), j: query))
+      [a, Image.new(j["image"], parent, client)]
+    end
+    
+    def rebuild(image:)
+      a , j = action(request(base_path("actions/rebuild"), j: {image: image}))
+      [a, j["root_password"]]
+    end
+
+    def change_type(server_type:, upgrade_disk: nil)
+      query = {}
+      method(:change_type).parameters.inject(query) do |r,x| 
+        (var = eval(x.last.to_s)).nil? ? r : r.merge!(x.last => var)
+      end
+      action(request(base_path("actions/change_type"), j: query))[0]
+    end
+    
+    def enable_backup(backup_window:)
+      action(request(base_path("actions/enable_backup"), 
+                     j: {backup_window: backup_window}))[0]
+    end
+    
+    def attach_iso(iso:)
+      action(request(base_path("actions/attach_iso"), 
+                     j: {iso: iso}))[0]
+    end
+    
+    %w(
+      poweron poweroff shutdown reboot reset 
+      disable_rescue disable_backup detach_iso
+    ).each do |action|
       define_method(action) do
-        Action.new(
-          Oj.load(request(base_path("actions/#{action}"), method: :post).run.body)["action"],
-          parent,
-          client
-        )
+        action(request(base_path("actions/#{action}"), method: :post))[0]
       end
     end
 
@@ -51,6 +92,14 @@ module Hcloud
     end
 
     private
+
+    def action(request)
+      j = Oj.load(request.run.body)
+      [
+        Action.new(j["action"],parent,client),
+        j
+      ]
+    end
 
     def base_path(ext=nil)
       return ["servers/#{id}",ext].compact.join("/") unless id.nil?
