833a57c45f3b5b8a96572db985fa9575664335eb
Tim Foerster <tim.foerster@hetzner.com>
2018-02-26 23:01:02 +0100

    Cover #authorized? from client
    Signed-off-by: Tim Foerster <tim.foerster@hetzner.com>

--- spec/hcloud/base_spec.rb
+++ spec/hcloud/base_spec.rb
@@ -1,9 +1,25 @@
 require 'spec_helper'
 
 describe "Generic" do
+  let :client do
+    Hcloud::Client.new(token: "secure")
+  end
+  
+  let :uclient do
+    Hcloud::Client.new(token: "invalid")
+  end
+
   it "preload all constants" do
     Hcloud.constants.each do |klass|
       Hcloud.send(:const_get, klass)
     end
+  end
+  
+  it "check authorized" do
+    expect(client.authorized?).to be(true)
+  end
+  
+  it "check unauthorized" do
+    expect(uclient.authorized?).to be(false)
   end
 end
