8fd7c00be3fa887031d197c5c58e31eb6b07b6e2
Tim Foerster <tim.foerster@hetzner.com>
2019-10-17 22:23:37 +0200

    Make sure all resources are loaded

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,6 +16,18 @@ require_relative './doubles/base'
 require 'rspec'
 require 'hcloud'
 
+def deep_load(scope)
+  return unless scope.respond_to?(:constants)
+
+  scope.constants.each do |const|
+    next unless scope.autoload?(const)
+
+    deep_load(scope.const_get(const))
+  end
+end
+
+deep_load Hcloud
+
 RSpec.configure do |c|
   Faker::Config.random = Random.new(c.seed)
 end
